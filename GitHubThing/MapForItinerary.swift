5919b5a815d369c6cbf514015fa4aab07301b488
Rosenfield, Preston M <190049@s020rmc148-21.local>
2018-11-27 09:15:40 -0600

    commit

---
+++ GitHubThing/MapForItinerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForItinerary.swift
+//  GitHubThing
+//
+//  Created by Rosenfield, Preston M on 11/27/18.
+//  Copyright © 2018 Rosenfield, Preston M. All rights reserved.
+//
+
+import Foundation
