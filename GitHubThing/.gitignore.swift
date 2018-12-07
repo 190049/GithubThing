450834667f369d74e0ce2d1ceeb3963593f9f09b
Rosenfield <190049@s020rmc148-21.ads.friscoisd.org>
2018-12-07 08:59:34 -0600

    Add .gitignore file

---
+++ GitHubThing/.gitignore.swift
@@ -0,0 +1,9 @@
+//
+//  .gitignore.swift
+//  GitHubThing
+//
+//  Created by Rosenfield, Preston M on 11/29/18.
+//  Copyright © 2018 Rosenfield, Preston M. All rights reserved.
+//
+
+import Foundation
